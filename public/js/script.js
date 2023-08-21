fcaaa4926ccef97bfc13dc541a905e30c533ad22
SJAcci <sungjuan1006@gmail.com>
2023-08-22 00:32:09 +0900

    update_1.1

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+function getTime(){
+    let clock = document.querySelector('.t_clock');
+    let date = new Date()
+        hours = date.getHours()
+        minutes = date.getMinutes();
+    let ampm = hours >= 12 ? '오후' : '오전';
+
+    hours = hours % 12 || 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    clock.innerHTML =  `${ampm}  ${hours}:${minutes}`;
+
+
+}
+
+window.addEventListener('load', function(){
+    getTime()
+
+    setInterval(getTime, 1000);
+})
